a6a10a21d46442245643ae49939c00bcff475215
Dsssyc <dsssyc02@gmail.com>
2026-04-08 22:25:39 +0800

    refactor(main): comment out flood map and video generation calls

--- main.py
+++ main.py
@@ -43,8 +43,8 @@ if __name__ == '__main__':
     evolve_domain_coupled(domain_alt, df7_cfg, pipe_cfg)
     
     # draw_hydrograph(domain_alt, 'D74', True, -3600)
-    generate_flood_map(domain_alt)
-    generate_flood_video(domain_alt, output_path='./resource/flood_video.mp4')
+    # generate_flood_map(domain_alt)
+    # generate_flood_video(domain_alt, output_path='./resource/flood_video.mp4')
     
     # preprocess(domain_mrcg, df7_cfg)
     # generate_max_inundation_extent_map(domain_4)
